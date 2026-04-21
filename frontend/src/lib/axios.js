afb0b010e60494daab8bbd472502f2b25f375d70
Saurabh <saurabh.anand122@gmail.com>
2026-04-21 13:18:20 +0530

    Use env backend URL in frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const BACKEND_BASE_URL = import.meta.env.VITE_REACT_APP_BACKEND_BASEURL;
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: `${BACKEND_BASE_URL}/api`,
   withCredentials: true,
 });
